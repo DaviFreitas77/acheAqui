721de97fbe9ec0c785b7cb848d526b93362cb540
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2024-09-27 15:04:07 -0300

    update

--- scr/pages/MeusPosts/index.js
+++ scr/pages/MeusPosts/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
+import { Context } from '../../context/provider';
 
 const MeusPosts = () => {
+  const {idUser,urlApi} = useContext(Context)
+  useEffect(()=>{
+    const fetchPost = async () =>{
+      try{
+        const response = await axios.get(`http://${urlApi}/services/getPostUsuario.php`,{
+          params:{id:idUser}
+        })
+
+        console.log(response.data)
+      }catch(error){
+        console.log(error)
+      }
+    
+    }
+    fetchPost()
+  },[])
+ 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
